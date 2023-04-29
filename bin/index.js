c81a8f1e3a1337a9198b08e5110edcc52c56ca79
Don Basil Peter <donbasilpeter@gmail.com>
2023-04-29 21:53:58 +0530

    added some styles

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,21 @@
 #! /usr/bin/env node
 
 const { program } = require('commander');
+const chalk = require('chalk');
 const { listFiles } = require('../utils/file');
+const packageDetails = require('../package.json');
 
 
 program
+  .version(packageDetails.version)
+  .description(packageDetails.description)
+  .name(packageDetails.name)
+  .addHelpCommand(true)
+
+program
   .command('listfiles')
-  .description('List all files in the current directory')
   .action(listFiles);
 
+
 program.parse();
+
